d1c149a500962665a7021dc2d4d60ad32b8ad4af
AbdullahAlTowsif <abdullah.al.towsif2002@gmail.com>
2025-06-17 00:22:53 +0600

    third party package validator & zod added

--- src/app/controllers/user.controller.ts
+++ src/app/controllers/user.controller.ts
@@ -1,67 +1,81 @@
 import express, { Request, Response } from "express";
 import { User } from "../models/user.model";
+import { z } from "zod";
 export const usersRoutes = express.Router();
 
+const CreateUserZodSchema = z.object({
+  firstName: z.string(),
+  lastName: z.string(),
+  age: z.number(),
+  email: z.string(),
+  password: z.string(),
+  role: z.string().optional(),
+});
 
 // create a user
-usersRoutes.post('/create-user', async (req:Request, res: Response) => {
-    const body = req.body;
+usersRoutes.post("/create-user", async (req: Request, res: Response) => {
+  try {
+    const body = await CreateUserZodSchema.parseAsync(req.body);
 
-    const user = await User.create(body)
+    const user = await User.create(body);
 
     res.status(201).json({
-        success: true,
-        message: "User created successfully",
-        user: user
-    })
-})
+      success: true,
+      message: "User created successfully",
+      user: user,
+    });
+
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 // get all users
-usersRoutes.get('/', async (req:Request, res: Response) => {
-    const users = await User.find()
+usersRoutes.get("/", async (req: Request, res: Response) => {
+  const users = await User.find();
 
-    res.status(201).json({
-        success: true,
-        message: "All users fetched successfully",
-        users
-    })
-})
+  res.status(201).json({
+    success: true,
+    message: "All users fetched successfully",
+    users,
+  });
+});
 
 // get single user by id
-usersRoutes.get('/:userId', async (req:Request, res: Response) => {
-    const userId = req.params.userId;
-    const user = await User.findById(userId)
+usersRoutes.get("/:userId", async (req: Request, res: Response) => {
+  const userId = req.params.userId;
+  const user = await User.findById(userId);
 
-    res.status(201).json({
-        success: true,
-        message: "User fetched successfully",
-        user
-    })
-})
+  res.status(201).json({
+    success: true,
+    message: "User fetched successfully",
+    user,
+  });
+});
 
 // update note by id
-usersRoutes.patch('/:userId', async (req:Request, res: Response) => {
-    const userId = req.params.userId;
-    const updatedBody = req.body;
-    const user = await User.findByIdAndUpdate(userId, updatedBody, {new: true})
+usersRoutes.patch("/:userId", async (req: Request, res: Response) => {
+  const userId = req.params.userId;
+  const updatedBody = req.body;
+  const user = await User.findByIdAndUpdate(userId, updatedBody, { new: true });
 
-    res.status(201).json({
-        success: true,
-        message: "User updated successfully",
-        user
-    })
-})
+  res.status(201).json({
+    success: true,
+    message: "User updated successfully",
+    user,
+  });
+});
 
 // delete user by id
-usersRoutes.delete('/:userId', async (req:Request, res: Response) => {
-    const userId = req.params.userId;
-    const user = await User.findByIdAndDelete(userId)
-    // const note = await User.deleteOne({_id: noteId})
-    // const note = await User.findOneAndDelete({_id: noteId})
+usersRoutes.delete("/:userId", async (req: Request, res: Response) => {
+  const userId = req.params.userId;
+  const user = await User.findByIdAndDelete(userId);
+  // const note = await User.deleteOne({_id: noteId})
+  // const note = await User.findOneAndDelete({_id: noteId})
 
-    res.status(201).json({
-        success: true,
-        message: "User deleted successfully",
-        user
-    })
-})
+  res.status(201).json({
+    success: true,
+    message: "User deleted successfully",
+    user,
+  });
+});
--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -1,21 +1,42 @@
 import { model, Schema } from "mongoose";
 import { IUser } from "../interfaces/user.interface";
+import validator from 'validator';
 
 const userSchema = new Schema<IUser>({
     firstName: {
         type: String,
         required: true,
-        trim: true
+        trim: true,
+        minlength: 4,
+        maxlength: 10
     },
     lastName: {
         type: String,
         required: true,
         trim: true
     },
+    age: {
+        type: Number,
+        required: true,
+        min: [18, 'Age MUST be at least 18, got {VALUE}'],
+        max: 60,
+    },
     email: {
         type: String,
         required: true,
-        trim: true
+        trim: true,
+        lowercase: true,
+        unique: [true, "Email must be unique!"],
+        // validate: {
+        //     validator: function(value) {
+        //         return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value)
+        //     },
+        //     message: function(props) {
+        //         return `Email ${props.value} is not valid email`
+        //     }
+        // }
+
+        validate: [validator.isEmail, "Invalid email {VALUE}"]
     },
     password: {
         type: String,
@@ -23,8 +44,12 @@ const userSchema = new Schema<IUser>({
     },
     role: {
         type: String,
-        enum: ['user', 'admin'],
-        default: 'user'
+        uppercase: true,
+        enum: {
+            values: ['USER', 'ADMIN', 'SUPERADMIN'],
+            message: "Role is not valid! got {VALUE}"
+        },
+        default: 'USER'
     }
 })
 
